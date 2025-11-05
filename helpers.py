ea4684f1ba52587e720f6580542befe34ff54dad
Евгений Коверев <ekoverev@ozon.ru>
2025-11-05 19:21:47 +0300

    remove shining data

--- helpers.py
+++ helpers.py
@@ -1,69 +1,11 @@
 # pylint: disable=line-too-long
 
 import config
-import database
 import keyboards
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 import datetime
 
 bot = config.bot
-
-
-def delete_users_dialog(bot, message):
-    answer = "Укажи логины пользователей, с которыми нужно прекратить общение (каждый логин в новой строке)"
-    msg = bot.send_message(chat_id=message.chat.id, text=answer)
-    bot.register_next_step_handler(msg, delete_users)
-
-
-def delete_users(message):
-    logins = message.text.split('\n')
-    logins = [each.strip() for each in logins]
-    names = database.get_name_by_login(logins)
-    names = [each[0] for each in names]
-    users_list_str = '\n* '.join(names)
-    answer = f"Ты хочешь удалить следующих пользователей: \n* {users_list_str} \n Все верно?"
-    msg = bot.send_message(chat_id=message.chat.id, text=answer, reply_markup=keyboards.Confirm.keyboard)
-    bot.register_next_step_handler(msg, confirm_delete, logins)
-
-
-def confirm_delete(message, logins):
-    if message.text == "Да":
-        database.delete_users(users=logins)
-        bot.send_message(chat_id=message.chat.id, text="Удалено", reply_markup=keyboards.MainMenuForAdmins.keyboard)
-    elif message.text == "Нет":
-        bot.send_message(chat_id=message.chat.id, text="Оставлены в чате",
-                         reply_markup=keyboards.MainMenuForAdmins.keyboard)
-
-
-def prepare_users_list_message():
-    all_users_with_notifications = database.get_all_users_full_data(enable_notifications=True)
-    print(all_users_with_notifications)
-    all_users_without_notifications = database.get_all_users_full_data(enable_notifications=False)
-    text_on = "✅ Список пользователей с включенными уведомлениями:"
-    for user in all_users_with_notifications:
-        if user[1] != "None":
-            user_tag = f"@{user[1]}"
-        else:
-            user_tag = user[2]
-        text_line = f"{user_tag}\n"
-        text_on += text_line
-    text_on += '\n\n'
-
-    text_off = "❌ Список пользователей с выключенными уведомлениями:"
-    for user in all_users_without_notifications:
-        if user[1] != "None":
-            user_tag = f"@{user[1]}"
-        else:
-            user_tag = user[2]
-        text_line = f"{user_tag}\n"
-        text_off += text_line
-    text_off += '\n\n'
-
-    count_data = (f"Всего пользователей: \n"
-                  f"* {len(all_users_with_notifications)} c уведомлениями \n"
-                  f"* {len(all_users_without_notifications)} без уведомлений")
-
-    return text_on + text_off + count_data
 
 
 def prepare_games_list_keyboard(games_json: dict) -> tuple[ReplyKeyboardMarkup, dict]:
--- main.py
+++ main.py
@@ -2,12 +2,10 @@ from time import sleep
 
 import config
 import constants
-import database
 import helpers
 import keyboards
 import requests
 
-# bot = telebot.TeleBot(config.Telegram.TOKEN)
 bot = config.bot
 
 
@@ -22,17 +20,16 @@ def start(message):
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
     if message.text == "Главное меню":
-        print("111")
         bot.send_message(
             chat_id=message.chat.id,
             text="Выберите функцию",
-            reply_markup=keyboards.NewMainMenu.keyboard
+            reply_markup=keyboards.MainMenu.keyboard
         )
     if message.text == "🗂 Таблица игр в GoogleSheets":
         bot.send_message(
             chat_id=message.chat.id,
-            text="Если нужна таблица в гугл таблицах, то вот ссылка <link>",
-            reply_markup=keyboards.NewMainMenu.keyboard
+            text=f"Если нужна таблица в гугл таблицах, то вот ссылка {constants.Links.team_table}",
+            reply_markup=keyboards.MainMenu.keyboard
         )
     elif message.text == "📇 Список ближайших игр":
         all_games_response = requests.get(config.TABLE_API.token)
@@ -57,106 +54,10 @@ def handle_text(message):
             bot.send_message(
                 chat_id=message.chat.id,
                 text=msg,
-                reply_markup=keyboards.NewMainMenu.keyboard,
+                reply_markup=keyboards.MainMenu.keyboard,
                 parse_mode="HTML"
             )
         
-
-@bot.message_handler(commands=['menu'])
-def menu(message):
-    reply_keyboard = keyboards.MainMenuForAdmins \
-        if message.from_user.username in config.ADMIN_USERS else keyboards.MainMenu
-    bot.send_message(message.chat.id, text="", reply_markup=reply_keyboard.keyboard)
-
-
-@bot.message_handler(content_types=["text"])
-def parse_text_register(message):  # noqa: C901
-    if message.text == "Авторизоваться":
-        if not database.check_user_already_exist(message.chat.id):
-            first_name = message.from_user.first_name if message.from_user.first_name else ""
-            last_name = message.from_user.last_name if message.from_user.last_name else ""
-            database.add_user(chat_id=message.chat.id,
-                              name=first_name + " " + last_name,
-                              login=message.from_user.username)
-            answer = constants.TextTemplates.message_after_auth
-        else:
-            answer = constants.TextTemplates.message_already_auth
-
-        reply_keyboard = keyboards.MainMenuForAdmins \
-            if message.from_user.username in config.ADMIN_USERS else keyboards.MainMenu
-        bot.send_message(chat_id=message.chat.id, text=answer, reply_markup=reply_keyboard.keyboard)
-
-    if message.text == "Сделать рассылку":
-        answer = "Укажи текст который нужно отправить"
-        msg = bot.send_message(chat_id=message.chat.id, text=answer, reply_markup=keyboards.Cansel.keyboard)
-        bot.register_next_step_handler(msg, send_mailing)
-
-    if message.text == "Удаление пользователей":
-        helpers.delete_users_dialog(bot, message)
-    if message.text == "Список участников":
-        prepared_text = helpers.prepare_users_list_message()
-        bot.send_message(chat_id=message.chat.id, text=prepared_text, reply_markup=keyboards.MainMenuForAdmins.keyboard)
-    if message.text == "🗂 Таблица игр":
-        bot.send_message(chat_id=message.chat.id, text=constants.TextTemplates.message_games_table,
-                         parse_mode='MarkdownV2', reply_markup=keyboards.MainMenu.keyboard)
-    if message.text == "📇 Презентация Сияния":
-        bot.send_message(chat_id=message.chat.id, text=constants.TextTemplates.message_team_promo,
-                         parse_mode='MarkdownV2', reply_markup=keyboards.MainMenu.keyboard)
-    if message.text == "🔗 Полезные ссылки":
-        bot.send_message(chat_id=message.chat.id, text=constants.TextTemplates.message_useful_links,
-                         parse_mode='MarkdownV2', reply_markup=keyboards.MainMenu.keyboard,
-                         disable_web_page_preview=True)
-    if message.text == "🍔 База едален":
-        bot.send_message(chat_id=message.chat.id, text=constants.TextTemplates.message_food,
-                         parse_mode='MarkdownV2', reply_markup=keyboards.MainMenu.keyboard)
-    if message.text == "✅ Включить рассылку о мероприятиях":
-        is_already_notified = database.check_user_notifications_value(chat_id=message.chat.id)
-        if is_already_notified:
-            bot.send_message(chat_id=message.chat.id, text=constants.TextTemplates.notifications_already_enabled,
-                             reply_markup=keyboards.MainMenu.keyboard)
-        else:
-            database.switch_notifications_flag(chat_id=message.chat.id, new_value=True)
-            bot.send_message(chat_id=message.chat.id, text=constants.TextTemplates.notifications_enabled,
-                             reply_markup=keyboards.MainMenu.keyboard)
-    if message.text == "❌ Отменить рассылку о мероприятиях":
-        is_already_notified = database.check_user_notifications_value(chat_id=message.chat.id)
-        if not is_already_notified:
-            bot.send_message(chat_id=message.chat.id, text=constants.TextTemplates.notifications_already_disabled,
-                             reply_markup=keyboards.MainMenu.keyboard)
-        else:
-            ans = bot.send_message(chat_id=message.chat.id,
-                                   text=constants.TextTemplates.message_notifications_off_confirm,
-                                   reply_markup=keyboards.Confirm.keyboard)
-            bot.register_next_step_handler(ans, confirm_notifications_off)
-
-
-def send_mailing(message):
-    all_users_chat_ids = database.get_all_users()
-    author = message.from_user.username
-    if message.text == "Отменить и вернуться в меню":
-        bot.send_message(
-            chat_id=message.chat.id, text="Возврат в меню", reply_markup=keyboards.MainMenuForAdmins.keyboard
-        )
-    else:
-        for chat_id in all_users_chat_ids:
-            print(all_users_chat_ids)
-            bot.send_message(chat_id=chat_id,
-                             text=constants.TextTemplates.template_for_mailing.format(
-                                 author=author, message=message.text)
-                             )
-            bot.send_message(chat_id=chat_id, text=constants.TextTemplates.send_answer_to,
-                             reply_markup=keyboards.MainMenu.keyboard)
-
-
-def confirm_notifications_off(message):
-    if message.text == "Да":
-        bot.send_message(chat_id=message.chat.id, text=constants.TextTemplates.notifications_disabled,
-                         reply_markup=keyboards.MainMenu.keyboard)
-        database.switch_notifications_flag(chat_id=message.chat.id, new_value=False)
-    else:
-        bot.send_message(chat_id=message.chat.id, text=constants.TextTemplates.notifications_not_disabled,
-                         reply_markup=keyboards.MainMenu.keyboard)
-
 
 while True:
     try:
